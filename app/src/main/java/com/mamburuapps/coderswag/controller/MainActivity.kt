dff5ffc953aa7ddf2a0e748dfc491de222257ed9
Mambu1002 <brushneede@gmail.com>
2022-04-27 11:10:50 +0300

    lesson3

--- app/src/main/java/com/mamburuapps/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/mamburuapps/coderswag/controller/MainActivity.kt
@@ -5,19 +5,18 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.mamburuapps.coderswag.R
 import com.mamburuapps.coderswag.Services.DaraService
+import com.mamburuapps.coderswag.adapters.CategoryAdapter
 import com.mamburuapps.coderswag.model.Category
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DaraService.categories)
+        adapter = CategoryAdapter(this, DaraService.categories)
         categoryListView.adapter = adapter
     }
 
